3220b6fb8e51808efef6220c5183bde465738a36
Mateus Paixao <mateus@MacBook-Air-de-NEONB1231.local>
2025-11-30 01:53:43 -0300

    validação da autenticação do usuário para o frontend respeitando documentação

--- src/app/validators/userSchemas.ts
+++ src/app/validators/userSchemas.ts
@@ -3,7 +3,9 @@ import { z } from 'zod';
 export const createUserSchema = z.object({
   name: z.string().min(3),
   email: z.string().email(),
-  phone: z.string().min(8).max(20).optional()
+  password: z.string().min(6),
+  phone: z.string().min(8).max(20).optional(),
+  roles: z.array(z.string().min(1)).optional()
 });
 
 export const updateUserSchema = createUserSchema.partial();
